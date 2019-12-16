e2c7a9a45e9f5234b0fa2619caa9c47cb4861ef5
Ammar Faizi <ammarfaizi2@gmail.com>
2019-12-16 19:09:53 +0700

    Clean up

--- pg/test.php
+++ pg/test.php
@@ -1,16 +1,20 @@
 <?php
+
 $runtime = new \parallel\Runtime();
+$runtime2 = new \parallel\Runtime();
 
 $future = $runtime->run(function(){
     for ($i = 0; $i < 500; $i++) {
         echo "*";
+        sleep(1);
     }
-
     return "easy";
 });
 
-for ($i = 0; $i < 500; $i++) {
-    echo ".";
-}
-
-printf("\nUsing \\parallel\\Runtime is %s\n", $future->value());
+$future2 = $runtime2->run(function(){
+    for ($i = 0; $i < 500; $i++) {
+        echo "|";
+        sleep(1);
+    }
+    return "easy";
+});
